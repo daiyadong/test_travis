a2f3ee2969bdb7d63e1e5d1323e8d4c8dee96703
daiyadong <mtnk2010@gmail.com>
2020-08-28 14:51:23 +0800

    new version.

--- hello.c
+++ hello.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
-
-int main()
+#include <curl/curl.h>
+ 
+int main(void)
 {
-	printf("hello, world!");
-	printf("This is a error");
+	CURL *curl;
+	CURLcode res;
+
+	curl = curl_easy_init();
+
+	if(curl) {
+		curl_easy_setopt(curl, CURLOPT_URL, "http://myip.ipip.net/");
+		res = curl_easy_perform(curl);
+
+		if(res != CURLE_OK) {
+			fprintf(stderr, "curl_easy_perform() failed: %s", curl_easy_strerror(res));
+		}
+
+		curl_easy_cleanup(curl);
+
+	}
+
 	return 0;
 }
